602e19d2b351bf272ae9094736a5d92a0c624a18
nosamni2 <nosmani2@illinois.edu>
2023-03-26 22:38:47 -0500

    personalizing ai image generator

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,16 +5,16 @@ function Header() {
   return (
     <header className="p-5 flex justify-between  sticky top-0 bg-white z-50 shadow-md">
       <div className="flex space-x-2 items-center">
-        <Image
+        {/* <Image
           src="https://links.papareact.com/4t3"
           alt="logo"
           height={30}
           width={30}
-        />
+        /> */}
         <div>
           <h1 className="font-bold">
-            The PAPAFAM <span className="text-violet-500">AI</span> Image
-            Generator 1234
+            Nafi Osmani <span className="text-violet-500">AI</span> Image
+            Generator
           </h1>
           <h2 className="text-xs">Powered by DALL·E 2 & Chat GPT</h2>
         </div>
@@ -22,13 +22,13 @@ function Header() {
 
       <div className="flex divide-x text-gray-500 items-center text-xs md:text-base">
         <Link
-          href="https://www.papareact.com"
+          href="https://osmani-portfolio.vercel.app/"
           className="px-2 font-light text-right"
         >
-          Join Zero to Full Stack Hero
+          Nafi Osmani
         </Link>
         <Link
-          href="https://links.papareact.com/microsoft-youtube-build"
+          href="https://github.com/nafiosmani/DALL-E2v2.0"
           className="px-2 font-light"
         >
           Github Repo
